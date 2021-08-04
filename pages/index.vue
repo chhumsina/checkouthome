9e2fbdbea744405190629b408191dc5f3bbe5ca5
CHHUM Sina <chhum.sina@ababank.com>
2021-08-04 09:54:33 +0700

    sdfsd

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template> 
+<div>
+<h2>HTML Iframes</h2>
+<p>You can use the height and width attributes to specify the size of the iframe:</p>
+
+<button @click="open=true">Open</button>
+
+<iframe v-if="open" src="https://checkoutiii-chhumsina.vercel.app/"  class="iframe" title="Iframe Example"></iframe>
+
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+open: false,
+    }
+  }
+}
+</script>
+
+<style>
+.iframe{
+  top: 0;
+  left: 0;
+  position: absolute;
+  width:100%; height: 100%;
+}
+</style>
